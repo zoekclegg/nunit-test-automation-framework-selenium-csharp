8f91e3faa26db96c8ac3de0230e033cb3906b6d6
Zoe <zclegg@hotmail.co.uk>
2021-11-02 20:59:51 +0000

    Added further Form Field tests

--- Main/Pages/FormFieldsPage.cs
+++ Main/Pages/FormFieldsPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.PageObjects;
 using System;
 
@@ -17,6 +18,14 @@ namespace AutomateNowDemo.Main.Pages
         [CacheLookup]
         private IWebElement InputField;
 
+        [FindsBy(How = How.Id, Using = "email")]
+        [CacheLookup]
+        private IWebElement EmailField;
+
+        [FindsBy(How = How.Id, Using = "contact-form-comment-message")]
+        [CacheLookup]
+        private IWebElement InputBoxField;
+
         [FindsBy(How = How.ClassName, Using = "pushbutton-wide")]
         [CacheLookup]
         private IWebElement SubmitButton;
@@ -25,9 +34,60 @@ namespace AutomateNowDemo.Main.Pages
         [CacheLookup]
         private IWebElement InputFieldSubmission;
 
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Checkboxes:')]")]
+        [CacheLookup]
+        private IWebElement CheckboxesSubmission;
+
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Radio Buttons:')]")]
+        [CacheLookup]
+        private IWebElement RadioButtonsSubmission;
+
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Dropdown Menu:')]")]
+        [CacheLookup]
+        private IWebElement DropdownMenuSubmission;
+
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Email:')]")]
+        [CacheLookup]
+        private IWebElement EmailSubmission;
+
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Input box:')]")]
+        [CacheLookup]
+        private IWebElement InputBoxSubmission;
+
         public void InputTextIntoInputField(String text)
         {
             InputField.SendKeys(text);
+        }
+
+        public bool IsInputFieldVisible()
+        {
+            return InputField.Displayed;
+        }
+
+        public void ClickCheckBox(int option)
+        {
+            driver.FindElement(By.XPath("//input[@value='Option " + option + "']")).Click();
+        }
+
+        public void ClickRadioButton(String option)
+        {
+            driver.FindElement(By.XPath("//input[@value='" + option + "']")).Click();
+        }
+
+        public void SelectDropdownOption(String option)
+        {
+            SelectElement DropdownMenu = new SelectElement(driver.FindElement(By.Id("g1103-dropdownmenu")));
+            DropdownMenu.SelectByText(option);
+        }
+
+        public void TypeInEmailField(String text)
+        {
+            EmailField.SendKeys(text);
+        }
+
+        public void TypeInInputBox(String text)
+        {
+            InputBoxField.SendKeys(text);
         }
 
         public void ClickSubmitButton()
@@ -39,5 +99,31 @@ namespace AutomateNowDemo.Main.Pages
         {
             return InputFieldSubmission.Text.Contains(text);
         }
+
+        public bool VerifyCheckboxText(String option)
+        {
+            return CheckboxesSubmission.Text.Contains(option);
+        }
+
+        public bool VerifyRadioButtonText(String option)
+        {
+            return RadioButtonsSubmission.Text.Contains(option);
+        }
+
+        public bool VerifyDropdownMenuText(String option)
+        {
+            return DropdownMenuSubmission.Text.Contains(option);
+        }
+
+        public bool VerifyEmailText(String option)
+        {
+            return EmailSubmission.Text.Contains(option);
+        }
+
+        public bool VerifyInputBoxText(String option)
+        {
+            return InputBoxSubmission.Text.Contains(option);
+        }
+
     }
 }
--- Test/Tests/FormFieldsTests.cs
+++ Test/Tests/FormFieldsTests.cs
@@ -18,14 +18,64 @@ namespace AutomateNowDemo
         }
 
         [Test]
-        public void VerifyInputFieldtext()
+        public void CannotSubmitEmptyInputField()
         {
             FormFieldsPage formFieldsPage = new FormFieldsPage(driver);
-            formFieldsPage.InputTextIntoInputField("Some text");
-            test.Log(Status.Pass, "Input Text inputted");
             formFieldsPage.ClickSubmitButton();
             test.Log(Status.Pass, "Form submitted");
-            Assert.That(formFieldsPage.VerifyInputFieldText("Some text"));
+            Assert.That(formFieldsPage.IsInputFieldVisible);
+            test.Log(Status.Pass, "Verify form not submitted");
+        }
+
+        [Test]
+        public void CannotSubmitInvalidEmail()
+        {
+            FormFieldsPage formFieldsPage = new FormFieldsPage(driver);
+            formFieldsPage.TypeInEmailField("email");
+            test.Log(Status.Pass, "Inputted invalid email");
+            formFieldsPage.ClickSubmitButton();
+            test.Log(Status.Pass, "Form submitted");
+            Assert.That(formFieldsPage.IsInputFieldVisible);
+            test.Log(Status.Pass, "Verify form not submitted");
+        }
+
+        [Test]
+        public void VerifyFormFields()
+        {
+            FormFieldsPage formFieldsPage = new FormFieldsPage(driver);
+            
+            formFieldsPage.InputTextIntoInputField("Some text");
+            test.Log(Status.Pass, "Input Text inputted");
+
+            formFieldsPage.ClickCheckBox(1);
+            formFieldsPage.ClickCheckBox(3);
+            test.Log(Status.Pass, "Clicked checkboxes");
+
+            formFieldsPage.ClickRadioButton("White");
+            test.Log(Status.Pass, "Clicked radio button");
+
+            formFieldsPage.SelectDropdownOption("Hexa");
+            test.Log(Status.Pass, "Selected dropdown option");
+
+            formFieldsPage.TypeInEmailField("test@test.com");
+            test.Log(Status.Pass, "Email inputted");
+
+            formFieldsPage.TypeInInputBox("Some description");
+            test.Log(Status.Pass, "Input Box text inputted");
+
+            formFieldsPage.ClickSubmitButton();
+            test.Log(Status.Pass, "Form submitted");
+            Assert.Multiple(() =>
+            {
+                Assert.That(formFieldsPage.VerifyInputFieldText("Some text"));
+                Assert.That(formFieldsPage.VerifyCheckboxText("1"));
+                Assert.That(formFieldsPage.VerifyCheckboxText("3"));
+                Assert.That(formFieldsPage.VerifyRadioButtonText("White"));
+                Assert.That(formFieldsPage.VerifyDropdownMenuText("Hexa"));
+                Assert.That(formFieldsPage.VerifyEmailText("test@test.com"));
+                Assert.That(formFieldsPage.VerifyInputBoxText("Some description"));
+            });
+            test.Log(Status.Pass, "Form fields verified");
         }
 
     }
