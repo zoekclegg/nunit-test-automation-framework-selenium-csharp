75cd9c15067c1cc25a1b787a480f9f465001a512
Zoe <zclegg@hotmail.co.uk>
2021-11-06 15:18:39 +0000

    Added Calendar Tests

---
+++ Main/Pages/CalendarPage.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+
+namespace AutomateNowDemo.Main.Pages
+{
+    class CalendarPage
+    {
+        IWebDriver driver;
+        public CalendarPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "g1065-selectorenteradate")]
+        [CacheLookup]
+        private IWebElement CalendarField;
+
+        [FindsBy(How = How.XPath, Using = "//span[contains(@class,'ui-datepicker-month')]")]
+        [CacheLookup]
+        private IWebElement CurrentMonth;
+
+        [FindsBy(How = How.XPath, Using = "//span[contains(@class,'ui-datepicker-year')]")]
+        [CacheLookup]
+        private IWebElement CurrentYear;
+
+        [FindsBy(How = How.XPath, Using = "//a[contains(@class,'ui-state-highlight')]")]
+        [CacheLookup]
+        private IWebElement HighLightedDay;
+
+        [FindsBy(How = How.XPath, Using = "//button[@type='submit']")]
+        [CacheLookup]
+        private IWebElement SubmitButton;
+
+        [FindsBy(How = How.XPath, Using = "//blockquote[@class='contact-form-submission']/p")]
+        [CacheLookup]
+        private IWebElement SubmittedDate;
+
+
+        
+
+        public void ClickCalendarField()
+        {
+            CalendarField.Click();
+        }
+
+        public void ClickSubmitButton()
+        {
+            SubmitButton.Click();
+        }
+
+        public void SelectDate(int date)
+        {
+            driver.FindElement(By.XPath($"(//a[contains(@class,'ui-state-default')])[{date}]")).Click();
+        }
+
+        public string GetSelectedDate()
+        {
+            return CalendarField.Text;
+        }
+
+        public string GetSubmittedDate()
+        {
+            return SubmittedDate.Text;
+        }
+
+        public List<String> GetHighlightedDate()
+        {
+            List<String> date = new List<string>() { };
+            ClickCalendarField();
+            date.Add(HighLightedDay.Text);
+            date.Add(CurrentMonth.Text);
+            date.Add(CurrentYear.Text);
+            return date;
+        }
+
+    }
+
+}
---
+++ Test/Tests/CalendarTests.cs
@@ -0,0 +1,57 @@
+﻿using AutomateNowDemo.Test.Base;
+using AutomateNowDemo.Main.Pages;
+using NUnit.Framework;
+using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+
+namespace AutomateNowDemo
+{
+
+    [TestFixture]
+    public class CalendarTests : BaseTest
+    {
+        [SetUp]
+        public void NavigateToCalendarsPage()
+        {
+            HomePage homePage = new HomePage(driver);
+            homePage.ClickNavButton("Calendars");
+            test.Log(Status.Pass, "Navigated to Calendars page");
+        }
+
+        [Test]
+        public void VerifyTodayIsHighlighted()
+        {
+            CalendarPage calendarPage = new CalendarPage(driver);
+            List<string> highlightedDate = calendarPage.GetHighlightedDate();
+
+            DateTime now = DateTime.Now;
+            string[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            string day = Convert.ToString(now.Day);
+            string month = months[now.Month-1];
+            string year = Convert.ToString(now.Year);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(day.Equals(highlightedDate[0]));
+                Assert.That(month.Equals(highlightedDate[1]));
+                Assert.That(year.Equals(highlightedDate[2]));
+            });
+            test.Log(Status.Pass, "Verified today is highlighted");
+        }
+
+        [Test]
+        [TestCase(15)]
+        public void CanSubmitDate(int date)
+        {
+            CalendarPage calendarPage = new CalendarPage(driver);
+            calendarPage.ClickCalendarField();
+            calendarPage.SelectDate(date);
+            string selectedDate = calendarPage.GetSelectedDate();
+            calendarPage.ClickSubmitButton();
+            Assert.That(calendarPage.GetSubmittedDate().Contains(selectedDate));
+            test.Log(Status.Pass, "Verified date submitted");
+        }
+
+    }
+}
